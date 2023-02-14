05e5da70b57bbd4d33d4a898686336685c016a55
daniel.lemos <daniellemos23@hotmail.com>
2023-02-14 14:45:17 -0500

    Face Recognition Initial

--- Back-End Projects/PokemonProject_DockerDjango/PokemonApp/PokemonApp/settings.py
+++ Back-End Projects/PokemonProject_DockerDjango/PokemonApp/PokemonApp/settings.py
@@ -76,18 +76,41 @@ WSGI_APPLICATION = 'PokemonApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pokemon_final_database',
         'USER': 'postgres',
         'PASSWORD': 'Runsho%232323',
-        'HOST': 'localhost',
+        'HOST': 'localhost', # db
         'PORT': '5432'
 
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
+    }
+}
+"""
+
+
+
 
 
 # Password validation
--- Back-End Projects/PythonProjects/14. Facial Recognition App/14. OOP Fourteenth Class - Facial Recognition.py
+++ Back-End Projects/PythonProjects/14. Facial Recognition App/14. OOP Fourteenth Class - Facial Recognition.py
@@ -1,6 +1,21 @@
 from cv2 import cv2
 import face_recognition as fr
 
+# Upload Images
+control_picture = fr.load_image_file('FotoA.jpg')
+trial_picture = fr.load_image_file('FotoB.jpg')
+
+# Pictures to RGB
+control_picture = cv2.cvtColor(control_picture, cv2.COLOR_BGR2RGB)
+trial_picture = cv2.cvtColor(trial_picture, cv2.COLOR_BGR2RGB)
+
+# Show Images
+cv2.imshow('Control Picture', control_picture)
+cv2.imshow('Trial Picture', trial_picture)
+
+# Keep the Image Alive
+cv2.waitKey(0)
+
 
 
 
