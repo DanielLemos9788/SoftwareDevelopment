791d97d38b7cb1383d6f78201f5258ae6090fb89
DanielLemos9788 <daniellemos23@hotmail.com>
2023-03-02 00:41:54 -0500

    Docker Real Update

--- Back-End Projects/PokemonProject_DockerDjango/PokemonApp/PokemonApp/settings.py
+++ Back-End Projects/PokemonProject_DockerDjango/PokemonApp/PokemonApp/settings.py
@@ -76,42 +76,26 @@ WSGI_APPLICATION = 'PokemonApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pokemon_final_database',
         'USER': 'postgres',
         'PASSWORD': 'Runsho%232323',
-        'HOST': 'localhost', # db
-        'PORT': '5432'
+        'HOST': 'db',
+        'PORT': '5435'
 
     }
 }
 """
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
-
 """
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'OPTIONS': {
-            'service': 'my_service',
-            'passfile': '.my_pgpass',
-        },
-    }
-}
-"""
-
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
