1b464c8427607b9dd5cd2775cf153aa6391ef53d
daniellemos <daniellemos23@hotmail.com>
2023-06-15 08:21:05 -0500

    Environment Plus fiRST Class

--- Back-End Projects/PythonProjects/14. Facial Recognition App/facial_recognition.py
+++ Back-End Projects/PythonProjects/14. Facial Recognition App/facial_recognition.py
@@ -9,6 +9,27 @@ trial_picture = fr.load_image_file('FotoB.jpg')
 control_picture = cv2.cvtColor(control_picture, cv2.COLOR_BGR2RGB)
 trial_picture = cv2.cvtColor(trial_picture, cv2.COLOR_BGR2RGB)
 
+# Location Control Face
+location_face_A = fr.face_locations(control_picture)[0]
+processed_face_A = fr.face_encodings(control_picture)[0]
+
+# Location Trial Face
+location_face_B = fr.face_locations(trial_picture)[0]
+processed_face_B = fr.face_encodings(trial_picture)[0]
+
+# Show Rectangle on Face
+cv2.rectangle(control_picture,
+              (location_face_A[3], location_face_A[0]),
+              (location_face_A[1], location_face_A[2]),
+              (0, 255, 0),
+              2)
+
+cv2.rectangle(trial_picture,
+              (location_face_B[3], location_face_B[0]),
+              (location_face_B[1], location_face_B[2]),
+              (0, 255, 0),
+              2)
+
 # Show Images
 cv2.imshow('Control Picture', control_picture)
 cv2.imshow('Trial Picture', trial_picture)
